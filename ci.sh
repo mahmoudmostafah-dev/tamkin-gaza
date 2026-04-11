89f228d54aa1ad23afbdb0a092890cd5edb09c76
abdelrhman-arfat <abdoarfat2006@gmail.com>
2026-04-11 15:32:46 +0200

    Mearge Setup i18n and shdadcn

--- ci.sh
+++ ci.sh
@@ -26,9 +26,6 @@ fi
 
 git commit -m "$COMMIT_MSG"
 
-echo "⬆️ Pushing branch..."
-git push origin "$NEW_BRANCH"
-
 echo "🔗 Creating Pull Request to $BASE_BRANCH..."
 gh pr create --base "$BASE_BRANCH" --head "$NEW_BRANCH" --title "$COMMIT_MSG"
 
