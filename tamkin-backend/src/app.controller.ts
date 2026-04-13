6f368fbc01f384a007f68e25394bcb34ad6b4a70
Mahmoud Zain <dev.mahmoud.zain@gmail.com>
2026-04-13 07:30:28 +0200

    remove spec file from guards

--- tamkin-backend/src/app.controller.ts
+++ tamkin-backend/src/app.controller.ts
@@ -1,8 +1,6 @@
 import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
-import { AuthenticationGuard } from './Common/Guards/Authentication/authentication.guard';
 import type { Request } from 'express';
-import { AuthorizationGuard } from './Common/Guards/Authorization/authorization.guard';
 import { E_UserRole } from './Common/Enums/user.enums';
 import { Auth } from './Common/Decorators/auth.decorator';
 import { E_TokenType } from './Common/Enums/token.enum';
@@ -11,11 +9,10 @@ import { E_TokenType } from './Common/Enums/token.enum';
 export class AppController {
   constructor(private readonly appService: AppService) { }
 
-  @Auth([E_UserRole.ADMIN], E_TokenType.ACCESS)
+  @Auth([E_UserRole.SUPER_ADMIN], E_TokenType.ACCESS)
   @Get()
   main(@Req() req: Request) {
     return this.appService.main(req);
   }
-
 
 }
